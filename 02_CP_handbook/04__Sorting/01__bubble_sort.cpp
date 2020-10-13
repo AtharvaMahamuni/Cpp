b3a51e2973514fab3d00e7a883ac9a0eb901d1b5
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-10-13 11:58:59 +0530

    sorting

--- 02_CP_handbook/04__Sorting/01__bubble_sort.cpp
+++ 02_CP_handbook/04__Sorting/01__bubble_sort.cpp
@@ -5,39 +5,34 @@ using namespace std;
 int main()
 {
     int n;
+    cout << "Enter the no. of Enetries: ";
     cin >> n;
-    int arr[n];
 
+    int arr[n];
+    cout << "Enter the set of elements: " << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    // for (int i : arr)
-    // {
-    //     cout << i << " ";
-    // }
-    // cout << endl;
-
-    //bubble sort:
+    //Bubble sort:
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < n - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 
-    for (int i : arr)
+    for (auto i : arr)
     {
         cout << i << " ";
     }
-    cout << endl;
 
     return 0;
 }
