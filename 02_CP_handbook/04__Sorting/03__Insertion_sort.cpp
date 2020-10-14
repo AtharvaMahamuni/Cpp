9632c3b8037db3d9d59a3012e2171b15359e3f0e
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-10-14 22:38:51 +0530

    insertion sort

--- 02_CP_handbook/04__Sorting/03__Insertion_sort.cpp
+++ 02_CP_handbook/04__Sorting/03__Insertion_sort.cpp
@@ -18,14 +18,13 @@ int main()
     for (int i = 0; i < n; i++)
     {
         int temp = arr[i];
-        int j = i;
-
-        while (arr[j] > temp && j >= 0)
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp)
         {
             arr[j + 1] = arr[j];
             j--;
         }
-        arr[j] = temp;
+        arr[j + 1] = temp;
     }
 
     for (int i : arr)
