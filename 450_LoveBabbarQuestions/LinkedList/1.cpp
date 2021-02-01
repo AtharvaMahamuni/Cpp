21094d2e5db2b0a1f00b755a52ac9ca5a9d07be2
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-02-01 17:31:18 +0530

    reversing a linked list

---
+++ 450_LoveBabbarQuestions/LinkedList/1.cpp
@@ -0,0 +1,90 @@
+/*
+Q. Given pointer to the head node of a linked list, the task is to reverse the linked list. We need to reverse the list by changing the links between nodes.
+
+
+Examples: 
+
+Input: Head of following linked list 
+1->2->3->4->NULL 
+Output: Linked list should be changed to, 
+4->3->2->1->NULL
+
+Input: Head of following linked list 
+1->2->3->4->5->NULL 
+Output: Linked list should be changed to, 
+5->4->3->2->1->NULL
+
+Input: NULL 
+Output: NULL
+*/
+
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+typedef struct node Node;
+
+Node *head = NULL, *tail = NULL, *p = NULL;
+
+void reverse()
+{
+    Node *prev = NULL, *curr = NULL;
+
+    // while (tail->next != NULL)
+    {
+    }
+}
+
+void insert(int n)
+{
+    p = new Node;
+    p->data = n;
+    if (head == NULL)
+    {
+        head = p;
+        tail = p;
+        head->next = NULL;
+    }
+    else
+    {
+        tail->next = p;
+        tail = p;
+        tail->next = NULL;
+    }
+}
+
+void traverse()
+{
+    if (head == NULL)
+    {
+        cout << "Linked list is empty" << endl;
+    }
+    else
+    {
+        p = head;
+        while (p != NULL)
+        {
+            cout << " " << p->data;
+            p = p->next;
+        }
+    }
+}
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    // for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+    for (int i : array)
+    {
+        insert(i);
+    }
+
+    traverse();
+
+    return 0;
+}
