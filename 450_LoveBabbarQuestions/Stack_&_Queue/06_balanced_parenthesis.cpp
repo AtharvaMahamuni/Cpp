5aa20656926eaf4d4eec25a529fe5cec609065cc
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-03-02 10:03:23 +0530

    Balnced paran issue to pop & compare

---
+++ 450_LoveBabbarQuestions/Stack_&_Queue/06_balanced_parenthesis.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+string stack;
+int top = -1;
+
+void push(char p)
+{
+    top++;
+    stack[top] = p;
+}
+
+char pop()
+{
+    char c;
+
+    c = stack[top];
+    top--;
+
+    return c;
+}
+
+bool flip(char p)
+{
+    if (p == ')')
+    {
+        return '(';
+    }
+    else if (p == ']')
+    {
+        return '[';
+    }
+    else
+    {
+        return '{';
+    }
+}
+
+bool checkParanthesis(string pattern)
+{
+    bool isIt = true;
+
+    for (int i = 0; (pattern[i] == '(' || pattern[i] == '[' || pattern[i] == '{'); i++)
+    {
+        cout << pattern[i] << endl;
+        push(pattern[i]);
+    }
+    // cout << pattern << endl;
+
+    //TODO: Write a code to pop and compare the paranthesis
+
+    return isIt;
+}
+
+int main()
+{
+    string pattern;
+
+    cout << "Enter the pattern of Paranthesis: ";
+    cin >> pattern;
+
+    if (checkParanthesis(pattern))
+    {
+        cout << "It is Balanced" << endl;
+    }
+    else
+    {
+        cout << "It is Unbalanced" << endl;
+    }
+
+    return 0;
+}
